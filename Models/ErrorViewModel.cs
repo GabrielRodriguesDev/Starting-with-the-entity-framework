80379202ab1d14a11653d07ed851b3e4d0f9c57a
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-10-13 22:36:43 -0300

    Starting Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Starting_with_the_entity_framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
