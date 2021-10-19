110a9f1e740950c4a34be22c40ea0137a16447a6
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2021-10-19 07:40:03 -0300

    Finalizando CRUD com o EF

--- Controllers/FuncionariosController.cs
+++ Controllers/FuncionariosController.cs
@@ -22,9 +22,35 @@ namespace Starting_with_the_entity_framework.Controllers
             return View();
         }
 
+        public IActionResult Editar(int id) {
+            Funcionario funcionario = this._database.Funcionarios.First(registro => registro.Id == id);
+            return View("Cadastrar", funcionario);
+        }
+
+        public IActionResult Deletar(int id) {
+            Funcionario funcionario = this._database.Funcionarios.First(registro => registro.Id == id);
+            this._database.Remove(funcionario);
+            this._database.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
         [HttpPost] // Método POST que será chamado pela view Cadastrar, e receberá um formulario (Objeto Funcionario já está sendo tipado direto na view)
         public IActionResult Salvar(Funcionario funcionario) {
-            this._database.Funcionarios.Add(funcionario); //Chamando o método add() que simula o insert, e como a classe é funcionarios como a Entidade no qual a tabela foi baseada passamos ela como data.
+            if(funcionario.Id == 0){ // == 0 Pois um valor do tipo int nunca é igual a null e sim a 0
+                // Caso não tenha o id ele salva um novo registro.
+                this._database.Funcionarios.Add(funcionario); //Chamando o método add() que simula o insert, e como a classe é funcionarios como a Entidade no qual a tabela foi baseada passamos ela como data.
+                
+            
+            } else {
+                // Caso tenha o id, ele atualiza o registro
+                Funcionario FuncionarioCadastrado = this._database.Funcionarios.First(registro => registro.Id == funcionario.Id);
+                // Atualizando registro que retornou do banco pela linha acima e atualizando as informações, pelas informações que foram obtidas pela view.
+                // Após realizar a mudança do objeto que foi retornado pelo banco (função First acima) ao aplicar o SaveChanges() ele já realiza o Update dentro do Banco de Dados
+                FuncionarioCadastrado.Nome = funcionario.Nome; 
+                FuncionarioCadastrado.Salario = funcionario.Salario;
+                FuncionarioCadastrado.Cpf = funcionario.Cpf;
+            }
             this._database.SaveChanges(); //Salvando as mudanças, simulando o commit
             return RedirectToAction("Index");
         }
